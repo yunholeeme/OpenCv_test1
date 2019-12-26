72b6a02406bab305009286f42d864a1dcc7c6386
hodu <neuezeal@gmail.com>
2019-12-26 19:15:24 +0900

    feat: Mat::row(), Mat::col()
    Mat::row(), Mat::col()

--- OpenCv_test/main.cpp
+++ OpenCv_test/main.cpp
@@ -6,17 +6,40 @@
 using namespace cv;
 using namespace std;
 
+void MatOp3();
+
 int main()
+{
+	MatOp3();
+
+	return 0;
+}
+
+void MatOp3()
 {
 	Mat img1 = imread("marco-assmann-zUMk4bg8GFc-unsplash.jpg");
 	Mat img2 = img1(Rect(700, 2000, 1920, 1080));
+	Mat img3 = img1(Rect(700, 2000, 1920, 1080)).clone();
+
+	Mat img4 = img1.rowRange(300, 500);
+	Mat img5 = img1.row(700);
+	Mat img6 = img1.row(800);
 
 	img2 -= ~img2;
 	namedWindow("img1", WINDOW_NORMAL);
 	namedWindow("img2", WINDOW_NORMAL);
+	namedWindow("img3", WINDOW_NORMAL);
+	namedWindow("img4", WINDOW_NORMAL);
+	namedWindow("img5", WINDOW_AUTOSIZE);
+	namedWindow("img6", WINDOW_AUTOSIZE);
+
 	imshow("img1", img1);
 	imshow("img2", img2);
-	waitKey(0);
-	return 0;
-}
+	imshow("img3", img3);
+	imshow("img4", img4);
+	imshow("img5", img5);
+	imshow("img6", img6);
 
+	waitKey(0);
+	destroyAllWindows();
+}
